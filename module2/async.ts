7a36d9926df4c6808f9cdbfca89cec3e8b97e380
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2024-05-06 15:29:16 +0600

    async promise

---
+++ module2/async.ts
@@ -0,0 +1,43 @@
+{
+  //  promise or async function
+
+  type SomeThing = string;
+
+  const createPromiseFn = (): Promise<SomeThing> => {
+    return new Promise<SomeThing>((resolve, reject) => {
+      const data: SomeThing = "hello world";
+      if (data) {
+        resolve(data);
+      } else {
+        reject("Reject hoye gache ");
+      }
+    });
+  };
+
+  const result = async (): Promise<SomeThing> => {
+    const data: SomeThing = await createPromiseFn();
+    return data;
+  };
+
+  //   result();
+
+  // fetching json data
+
+  interface Todo {
+    user: string;
+    id: number;
+    title: string;
+    // completed: string;  // why not getting error ,,,,  data যদি বড় হয় তাহলে কি করবে ?
+  }
+
+  const fetchData = async (): Promise<Todo> => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    const data = await res.json();
+    console.log(data);
+
+    return data;
+  };
+  fetchData();
+
+  //
+}
